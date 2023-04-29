1cedf318e0f29bd63f6e2aa4859e7d33fb5acde3
Anika-Purohit <anikapurohit6@gmail.com>
2023-04-29 12:03:08 +0530

    dummy

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-green-300 grid grid-cols-4 items-center h-20">
+     <span >Instagram Link</span> 
+     <span>Facebook  Link</span> 
+     <span>Twitter Link</span> 
+     <span>NewsLetter Link</span> 
+    </div>
+  )
+}
+
+export default Footer
